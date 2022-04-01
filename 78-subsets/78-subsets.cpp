c6fdee1bf8d2ab3d34179375140acb6bfa155b85
Prasad Khalkar <aniketkhalkar791@gmail.com>
2022-04-01 16:11:47 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (41.57%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -15,8 +15,23 @@ public:
         help(nums,arr,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;
-        help(nums,temp,0);
+        // vector<int> temp;
+        // help(nums,temp,0);
+        // return ans;
+        
+        int n = nums.size();
+        int total = (1<<n) - 1;
+        
+        for(int i=0;i<=total;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                if(i & 1<<j){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        
         return ans;
     }
 };
