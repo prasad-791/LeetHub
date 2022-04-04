157b946399a311607edf518118e6c7f81b23cb0c
Prasad Khalkar <aniketkhalkar791@gmail.com>
2022-04-04 18:39:43 +0530

    Time: 1080 ms (20.22%), Space: 190.2 MB (5.62%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,28 +10,53 @@
  */
 class Solution {
 public:
+//     ListNode* swapNodes(ListNode* head, int k) {
+//         stack<ListNode*> st;
+//         ListNode* p = head, * q = NULL;
+//         int x = 0;
+//         while(p!=NULL){
+//             st.push(p);
+//             x++;
+//             if(x==k){
+//                 q = p;
+//             }
+//             p = p->next;
+//         }
+        
+//         while(!st.empty() && k){
+//             p = st.top();
+//             st.pop();
+//             k--;
+//         }
+        
+//         x = p->val;
+//         p->val = q->val;
+//         q->val = x;
+//         return head;
+//     }
+    
     ListNode* swapNodes(ListNode* head, int k) {
-        stack<ListNode*> st;
-        ListNode* p = head, * q = NULL;
-        int x = 0;
+        vector<int> arr;
+        ListNode* p = head;
+        int n = 0;
         while(p!=NULL){
-            st.push(p);
-            x++;
-            if(x==k){
-                q = p;
-            }
+            arr.push_back(p->val);
+            p = p->next;
+            n++;
+        }
+        int f = k-1;
+        int l = (n-1)-(k-1);
+        
+        int x = arr[f];
+        arr[f] = arr[l];
+        arr[l] = x;
+        
+        p = head;
+        x = 0;
+        while(p!=NULL){
+            p->val = arr[x++];
             p = p->next;
         }
-        
-        while(!st.empty() && k){
-            p = st.top();
-            st.pop();
-            k--;
-        }
-        
-        x = p->val;
-        p->val = q->val;
-        q->val = x;
         return head;
     }
 };
